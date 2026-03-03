898dc13e970934cc509ffa2af49f81d08d75219f
Harsh Sharma <mailittoharsh@gmail.com>
2026-03-03 19:41:21 +0530

    added card elements and their own pages

---
+++ script.js
@@ -0,0 +1,15 @@
+let allElems = document.querySelectorAll(".elem")
+let allFullElems = document.querySelectorAll(".fullElem")
+let allFullElemsBackBtn = document.querySelectorAll(".fullElem .back")
+
+allElems.forEach(function(elem){
+    elem.addEventListener('click',function(){
+        allFullElems[elem.id].style.display = 'block'
+    })
+})
+
+allFullElemsBackBtn.forEach(function(back){
+    back.addEventListener('click',function(){
+        allFullElems[back.id].style.display = 'none'
+    })
+})
