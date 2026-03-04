c79380b8c87ffd5d3344ca5d11faca1181ca7d8b
Harsh Sharma <mailittoharsh@gmail.com>
2026-03-05 00:20:13 +0530

    Made To-do list UI

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-let allElems = document.querySelectorAll(".elem")
-let allFullElems = document.querySelectorAll(".fullElem")
-let allFullElemsBackBtn = document.querySelectorAll(".fullElem .back")
+function openFeatures(){
+    let allElems = document.querySelectorAll(".elem")
+    let allFullElems = document.querySelectorAll(".fullElem")
+    let allFullElemsBackBtn = document.querySelectorAll(".fullElem .back")
 
-allElems.forEach(function(elem){
-    elem.addEventListener('click',function(){
-        allFullElems[elem.id].style.display = 'block'
+    allElems.forEach(function(elem){
+        elem.addEventListener('click',function(){
+            allFullElems[elem.id].style.display = 'block'
     })
 })
 
-allFullElemsBackBtn.forEach(function(back){
-    back.addEventListener('click',function(){
-        allFullElems[back.id].style.display = 'none'
+    allFullElemsBackBtn.forEach(function(back){
+        back.addEventListener('click',function(){
+            allFullElems[back.id].style.display = 'none'
     })
 })
+}
+
+openFeatures();
